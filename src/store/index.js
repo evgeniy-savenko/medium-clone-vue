12d8eec4afd6680c42268cf05f543a5a3a7a38d7
Evgeniy Savenko <wayrez27@gmail.com>
2022-11-08 19:40:14 +0300

    add: article page

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import {createStore} from 'vuex';
 import {auth} from '@/store/modules/auth';
 import {feed} from '@/store/modules/feed';
-import {popularTags} from './modules/popularTags';
+import {popularTags} from '@/store/modules/popularTags';
+import {article} from '@/store/modules/article';
 
 export default createStore({
   modules: {
     auth: auth,
     feed: feed,
     popularTags: popularTags,
+    article: article,
   },
 });
---
+++ src/views/Article.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="article-page">
+    <div class="banner">
+      <div class="container" v-if="article">
+        <h1>{{ article.title }}</h1>
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username},
+              }"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <span>
+            <router-link
+              class="btn btn-outline-secondary btn-sm"
+              :to="{name: 'editArticle', params: {slug: article.slug}}"
+            >
+              <i class="iod-edit"> Edit Article </i>
+            </router-link>
+            <button class="btn btn-outline-danger btn-sm">
+              <i class="ion-trash-a"> Delete Article </i>
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="container page">
+      <mcv-loading v-if="isLoading" />
+      <mcv-error-message v-if="error" />
+      <div class="row article-content" v-if="article">
+        <div class="col-xs-12">
+          <div>
+            <p>{{ article.body }}</p>
+          </div>
+          TAGLIST
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {actionTypes} from '@/store/modules/article';
+import {mapState} from 'vuex';
+
+export default {
+  name: 'McvArticle',
+  computed: {
+    ...mapState({
+      isLoading: (state) => state.article.isLoading,
+      error: (state) => state.article.error,
+      article: (state) => state.article.data,
+    }),
+  },
+  mounted() {
+    console.log(this.$route);
+    this.$store.dispatch(actionTypes.getArticle, {
+      slug: this.$route.params.slug,
+    });
+  },
+};
+</script>
+
+<style></style>
